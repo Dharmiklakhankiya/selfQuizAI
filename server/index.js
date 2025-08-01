ebf0845ea27fb47b0d19db5ebf95ccbfa15b3d18
Dharmik Lakhankiya <dharmiklakhankiya.com>
2025-08-01 18:18:21 +0530

    configure cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ import authRoutes from "./routes/authRoutes.js"; // Import auth routes
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // Connect to Mongo
